9f84320b11ef0c7c404e9f390432e38d935d45fd
Aleah Connley <aleahkc@gmail.com>
2020-06-25 12:51:40 +0200

    Office outed

--- 7-8kyu.js
+++ 7-8kyu.js
@@ -684,3 +684,82 @@ function infected3(s) {
 }
 
 //console.log(infected3("01000000X000X011X0X")); //Answer: 73.33333333333333
+
+//THE OFFICE I - OUTED
+/* 
+https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1
+Your colleagues have been looking over you shoulder. When you should have been doing your boring real job, 
+you've been using the work computers to smash in endless hours of codewars.
+In a team meeting, a terrible, awful person declares to the group that you aren't working. You're in trouble. 
+You quickly have to gauge the feeling in the room to decide whether or not you should gather your things and leave.
+Given an object (meet) containing team member names as keys, and their happiness rating out of 10 as the value, 
+you need to assess the overall happiness rating of the group. If <= 5, return 'Get Out Now!'. 
+Else return 'Nice Work Champ!'.
+Happiness rating will be total score / number of people in the room.
+Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
+*/
+
+const meet = {
+  tim: 0,
+  jim: 2,
+  randy: 0,
+  sandy: 7,
+  andy: 0,
+  katie: 5,
+  laura: 1,
+  saajid: 2,
+  alex: 3,
+  john: 2,
+  mr: 0,
+};
+const boss = "laura";
+
+function outed(meetObject, bossString) {
+  let totalScore = 0;
+  for (const prop in meetObject) {
+    console.log(prop, meetObject[prop]);
+    if (prop === bossString) {
+      totalScore += meetObject[prop] * 2;
+    } else {
+      totalScore += meetObject[prop];
+    }
+  }
+  if (totalScore / Object.keys(meetObject).length <= 5) {
+    return "Get Out Now!";
+  } else {
+    return "Nice Work Champ!";
+  }
+}
+
+function outed2(meetObject, bossString) {
+  let totalScore = 0;
+  for (const prop in meetObject) {
+    totalScore += meetObject[prop];
+  }
+  if (
+    (totalScore + meetObject[bossString]) / Object.keys(meetObject).length <=
+    5
+  ) {
+    return "Get Out Now!";
+  } else {
+    return "Nice Work Champ!";
+  }
+}
+
+function outed3(meetObject, bossString) {
+  const totalScore = Object.keys(meetObject).reduce((sum, name) => {
+    if (name === bossString) return sum + meetObject[name] * 2;
+    else return sum + meetObject[name];
+  }, 0);
+
+  if (totalScore / Object.keys(meetObject).length <= 5) return "Get Out Now!";
+  else return "Nice Work Champ!";
+}
+
+function outed4(meet, boss) {
+  let names = Object.keys(meet);
+  let score = names.reduce((s, v) => s + meet[v], 0) + meet[boss];
+  return score / names.length > 5 ? "Nice Work Champ!" : "Get Out Now!";
+}
+
+// console.log(outed3(meet, boss));
