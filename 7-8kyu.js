07ac72ff268b3bc42c2ae8a60cc6dd67eff25938
Aleah Connley <aleahkc@gmail.com>
2020-06-27 15:39:23 +0200

    add Office II

--- 7-8kyu.js
+++ 7-8kyu.js
@@ -763,3 +763,143 @@ function outed4(meet, boss) {
 }
 
 // console.log(outed3(meet, boss));
+
+//THE OFFICE II - BOREDOM SCORE
+/*
+https://www.codewars.com/kata/57ed4cef7b45ef8774000014
+Every now and then people in the office moves teams or departments. 
+Depending what people are doing with their time they can become more or less boring. 
+Time to assess the current team.
+You will be provided with an object(staff) containing the staff names as keys, and the 
+department they work in as values.
+Each department has a different boredom assessment score, as follows:
+accounts = 1
+finance = 2
+canteen = 10
+regulation = 3
+trading = 6
+change = 6
+IS = 8
+retail = 5
+cleaning = 4
+pissing about = 25
+Depending on the cumulative score of the team, return the appropriate sentiment:
+<=80: 'kill me now'
+< 100 & > 80: 'i can handle this'
+100 or over: 'party time!!'
+*/
+const staff = {
+  tim: "IS",
+  jim: "finance",
+  randy: "pissing about",
+  sandy: "cleaning",
+  andy: "cleaning",
+  katie: "cleaning",
+  laura: "pissing about",
+  saajid: "regulation",
+  alex: "regulation",
+  john: "accounts",
+  mr: "canteen",
+};
+function boredom(staff) {
+  let boredomScore = 0;
+  for (prop in staff) {
+    switch (staff[prop]) {
+      case "accounts":
+        boredomScore += 1;
+        break;
+      case "finance":
+        boredomScore += 2;
+        break;
+      case "canteen":
+        boredomScore += 10;
+        break;
+      case "regulation":
+        boredomScore += 3;
+        break;
+      case "trading":
+        boredomScore += 6;
+        break;
+      case "change":
+        boredomScore += 6;
+        break;
+      case "IS":
+        boredomScore += 8;
+        break;
+      case "retail":
+        boredomScore += 5;
+        break;
+      case "cleaning":
+        boredomScore += 4;
+        break;
+      case "pissing about":
+        boredomScore += 25;
+        break;
+      default:
+        boredomScore += 0;
+        break;
+    }
+  }
+  if (boredomScore <= 80) return "kill me now";
+  if (boredomScore < 100 && boredomScore > 80) return "i can handle this";
+  if (boredomScore >= 100) return "party time!!";
+}
+
+const activityScoresObj = {
+  accounts: 1,
+  finance: 2,
+  canteen: 10,
+  regulation: 3,
+  trading: 6,
+  change: 6,
+  IS: 8,
+  retail: 5,
+  cleaning: 4,
+  "pissing about": 25,
+};
+
+function boredom2(staff) {
+  const activityScoresObj = {
+    accounts: 1,
+    finance: 2,
+    canteen: 10,
+    regulation: 3,
+    trading: 6,
+    change: 6,
+    IS: 8,
+    retail: 5,
+    cleaning: 4,
+    "pissing about": 25,
+  };
+  var score = Object.keys(staff).reduce((sum, curVal) => {
+    // console.log(sum, curVal, staff[curVal], boredomObject[staff[curVal]]);
+    return sum + activityScoresObj[staff[curVal]];
+  }, 0);
+
+  if (score <= 80) return "kill me now";
+  if (score > 100) return "party time!!";
+  return "i can handle this";
+}
+
+function boredom3(staff) {
+  let boredomLevel = 0;
+  for (let person in staff) {
+    boredomLevel += activityScoresObj[staff[person]];
+    // console.log(person, staff[person], activityScoresObj[staff[person]]);
+  }
+  if (boredomLevel <= 80) return "kill me now";
+  else if (boredomLevel > 80 && boredomLevel < 100) return "i can handle this";
+  else return "party time!!";
+}
+
+function boredom4(staff) {
+  let boredemLevel = 0;
+  Object.values(staff).forEach(function (department) {
+    boredemLevel += activityScoresObj[department];
+    console.log(department);
+  });
+  if (boredemLevel <= 80) return "kill me now";
+  else if (boredemLevel > 80 && boredemLevel < 100) return "i can handle this";
+  else return "party time!!";
+}
+console.log(boredom4(staff));
