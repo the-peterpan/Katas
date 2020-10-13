fdaea1e992ef304145884441487073ac4c97f0c7
Aleah Connley <aleahkc@gmail.com>
2020-10-13 11:14:28 +0200

    Add McClock-Face'

--- 6kyu.js
+++ 6kyu.js
@@ -626,4 +626,43 @@ function lastDayIsFriday(initialYear, endYear) {
   return counter;
 }
 
-console.log(lastDayIsFriday(1901, 2000)); //171
+// console.log(lastDayIsFriday(1901, 2000)); //171
+
+//CLOCKY McCLOCK-FACE
+/*
+https://www.codewars.com/kata/clocky-mc-clock-face/javascript
+Due to lack of maintenance the minute-hand has fallen off Town Hall clock face.
+And because the local council has lost most of our tax money to a Nigerian email scam there 
+are no funds to fix the clock properly.
+Instead, they are asking for volunteer programmers to write some code that tell the 
+time by only looking at the remaining hour-hand!
+What a bunch of cheapskates!
+Can you do it? (0 <= angle <= 360)
+Examples:
+12:00 = 0 degrees
+02:00 = 60 degrees
+03:00 = 90 degrees
+06:00 = 180 degrees
+09:00 = 270 degrees
+12:00 = 360 degrees
+*/
+var whatTimeIsIt = function(angle) {
+  var allMinutes = Math.trunc(angle*2)
+  var minutes = (allMinutes % 60) + ""
+  if(minutes.length === 1){
+      minutes  = 0 + minutes
+    }
+  var hours = (allMinutes - minutes) / 60
+  var hoursString =  hours.toString()
+  if(hoursString.length === 1){
+      hoursString = 0 + hoursString
+    }
+  if(hoursString === "00"){
+   hoursString = "12"
+    }
+  
+  var time = hoursString + ":" + minutes
+  return time;
+  }
+
+  // console.log(whatTimeIsIt(40));
