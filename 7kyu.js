4e80f2963f77815755db31f7e598f28043b9cb4c
Aleah Connley <aleahkc@gmail.com>
2020-10-27 11:29:10 +0100

    add removeConsecDupes

--- 7kyu.js
+++ 7kyu.js
@@ -1,3 +1,4 @@
+//MISSING VALUES
 
 function missingValues(arr) {
   let resultObj = {};
@@ -22,4 +23,24 @@ function missingValues2(arr) {
   const y = arr.find( v => arr.filter( w => v===w ).length===2 );
   return x*x*y;
 }
-console.log(missingValues2([42, 23, 45, 33, 33, 19, 42, 79, 79, 23, 95, 95, 79, 19, 42, 33, 19, 23])); //192375
+// console.log(missingValues2([42, 23, 45, 33, 33, 19, 42, 79, 79, 23, 95, 95, 79, 19, 42, 33, 19, 23])); //192375
+
+
+//REMOVE CONSECUTIVE DUPLICATE WORDS
+/*
+https://www.codewars.com/kata/5b39e91ee7a2c103300018b3/train/javascript
+Your task is to remove all consecutive duplicate words from string, 
+leaving only first words entries. For example:
+"alpha beta beta gamma gamma gamma delta alpha beta beta
+ gamma gamma gamma delta"
+
+--> "alpha beta gamma delta alpha beta gamma delta"
+*/
+
+const removeConsecutiveDuplicates = s => { 
+  wordArr = s.split(' ');
+  const result = (wordArr.filter((curWord, index) => curWord !== wordArr[index+1])).join(' ');
+  return result;
+}
+
+// console.log(removeConsecutiveDuplicates("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
