459b0ca2e1fb4c8f7f97be72d84182337d3c3b12
Aleah Connley <aleahkc@gmail.com>
2020-10-28 14:06:14 +0100

    add Get the Middle Character

--- 7kyu.js
+++ 7kyu.js
@@ -44,3 +44,24 @@ const removeConsecutiveDuplicates = s => {
 }
 
 // console.log(removeConsecutiveDuplicates("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"));
+
+//GET THE MIDDLE CHARACTER
+/*
+https://www.codewars.com/kata/56747fd5cb988479af000028
+
+ou are going to be given a word. Your job is to return the middle character
+of the word. If the word's length is odd, return the middle character. 
+If the word's length is even, return the middle 2 characters.
+
+#Examples:
+Kata.getMiddle("test") should return "es"
+Kata.getMiddle("testing") should return "t"
+*/
+
+function getMiddle(s) {
+  let middle = s.length/2;
+  if (s.length % 2 === 0) return ([...s].splice(Math.floor((middle-1)), 2)).join('')
+  else return s.charAt(middle);
+}
+
+console.log(getMiddle("middle"));
