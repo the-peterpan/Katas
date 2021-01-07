2085ed3703659d42cfce66b094858b530fc08c50
Aleah Connley <aleahkc@gmail.com>
2021-01-07 12:10:20 +0100

    Money Money Money done

--- 7kyu.js
+++ 7kyu.js
@@ -301,7 +301,52 @@ var isSquare = function(n){
   return Number.isInteger(squareRoot);
 }
 
-console.log(isSquare(0)); //true
-console.log(isSquare(3)); //false
-console.log(isSquare(25)); //true
-console.log(isSquare(-1)); //false
+// console.log(isSquare(0)); //true
+// console.log(isSquare(3)); //false
+// console.log(isSquare(25)); //true
+// console.log(isSquare(-1)); //false
+
+
+//Money, Money, Money
+
+/*
+https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript
+Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+Example:
+
+  Let P be the Principal = 1000.00      
+  Let I be the Interest Rate = 0.05      
+  Let T be the Tax Rate = 0.18      
+  Let D be the Desired Sum = 1100.00
+
+
+After 1st Year -->
+  P = 1041.00
+After 2nd Year -->
+  P = 1083.86
+After 3rd Year -->
+  P = 1128.30
+*/
+
+function calculateYears(principal, interest, tax, desired) {
+  let years = 0;
+  if (principal >= desired) return 0;
+  do {
+    let accruedInterest = principal*interest;
+    let yearlyTax = (principal*interest)*tax;
+    principal += accruedInterest - yearlyTax;
+    years++;
+  }
+  while (principal < desired);
+  
+  return years;
+}
+
+console.log(calculateYears(1000, .05, .18, 1100)); //3
+console.log(calculateYears(1000, 0.01625,0.18,1200)); //14
+console.log(calculateYears(1000, 0.05,0.18,1000)); //0
