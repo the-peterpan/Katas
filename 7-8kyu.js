c525053e11923bd1418c1a71115c03dd7c376e34
Aleah Connley <aleahkc@gmail.com>
2021-07-20 05:13:42 +0200

    add divisors

--- 7-8kyu.js
+++ 7-8kyu.js
@@ -938,5 +938,59 @@ else return 'Please enter a floor number'
 //Refactored sol'n:
 const getRealFloor2 = (n) => (n<=0) ? n : (n<13) ? n-1 : n-2; 
 
-console.log(getRealFloor2(1)); //0
-console.log(getRealFloor2(5)) ;//4
+// console.log(getRealFloor2(1)); //0
+// console.log(getRealFloor2(5)) ;//4
+
+//FIND THE DIVISORS
+/*
+https://www.codewars.com/kata/544aed4c4a30184e960010f4
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+*/
+
+// KISS SOL'N:
+function divisors(integer) {
+  let result = [];
+  if (integer > 1) {
+    for (i=2; i<=integer/2; i++) {
+    if (Number.isInteger(integer/i)) {
+    result.push(i);
+      }
+    } 
+  if (result.length === 0) return `${integer} is prime`
+  
+  else return result;
+
+} 
+else return 'Please enter a number greater than 1';
+};
+
+//REFACTORED SOL'N:
+const divisors2 = (integer) => {
+  
+  const result = [];
+  const integerArr = Array.from({ length: integer/2 + 1}, (item, index) => index);  
+
+  integerArr.forEach((element) => {
+    if ((element > 1) && (Number.isInteger(integer/element)))
+      result.push(element);
+  });
+
+  if (result.length === 0) return `${integer} is prime`;
+
+  else return result;
+}
+
+
+//Alt. refactored sol'n:
+function divisors(integer) {
+  var res = []
+  for (var i = 2; i <= Math.floor(integer / 2); ++i) if (integer % i == 0) res.push(i);
+  return res.length ? res : integer + ' is prime'
+};
+
+console.log(divisors2(12)); // [2, 3 4, 6]
