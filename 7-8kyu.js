a2dd4e0e3de582e6b5214257a360e401192f688e
Aleah Connley <aleahkc@gmail.com>
2021-07-22 02:39:20 +0200

    add min max values

--- 7-8kyu.js
+++ 7-8kyu.js
@@ -1032,4 +1032,40 @@ function overTheRoad2(address, n){
   return (n*2+1)-address;
 }
 
-console.log(overTheRoad2(1,3)); //6
+// console.log(overTheRoad2(1,3)); //6
+
+
+//THE HIGHEST PROFIT WINS
+/*
+Story
+Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples
+minMax([1,2,3,4,5])   == [1,5]
+minMax([2334454,5])   == [5, 2334454]
+minMax([1])           == [1, 1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+*/
+
+function minMax(arr){
+  const minVal = Math.min(...arr);
+  const maxVal = Math.max(...arr);
+  return [minVal,maxVal];
+}
+
+//Refactored sol'n:
+function minMax2(arr){
+  return [Math.min(...arr),Math.max(...arr)];
+}
+
+//Sol'n using sort:
+function minMax3(arr){
+  arr.sort((a,b) => a-b)
+  return [arr[0], arr[arr.length-1]];
+}
+
+console.log(minMax3([1,3,4,2,5])); // [1,5]
